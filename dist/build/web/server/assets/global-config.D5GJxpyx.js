a327d38c50b4e4902b01dedba3dddeed208e4e1f
zhao yitao <johocn@163.com>
2026-08-16 12:20:09 +0800

    chore(admin): 提交 H5 后台构建产物 dist/build

--- dist/build/web/server/assets/global-config.D5GJxpyx.js
+++
@@ -1,1 +0,0 @@
-"use strict";const t=require("./request.btngW0ic.js"),e=require("./format.QYE0YuBS.js"),o="/zhao-common/v1/admin/global-config";exports.getGlobalConfig=function(){return t.get(o).then(e.extractItem)},exports.updateGlobalConfig=function(r){return t.put(o,r).then(e.extractItem)};
